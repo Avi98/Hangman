a2664f74693bb8d67dfbb35283ba12cb0ca85fe2
Avinash <avinash.kumar@renegadeinsurance.com>
2024-02-14 23:10:01 +0530

    chore: fixed types

--- apps/web/src/store/utility/expandType.ts
+++ apps/web/src/store/utility/expandType.ts
@@ -4,14 +4,6 @@ export type expandedTypeRecursively<T> = T extends object
     : never
   : T;
 
-type Mutate<S> = S extends object
-  ? { -readonly [K in keyof S]: Mutate<S[K]> }
-  : S extends boolean
-    ? boolean
-    : S extends string
-      ? string
-      : S;
-
 type Immutable<S> = { readonly [K in keyof S]: Immutable<S[K]> };
 
-export type ExtractType<S> = expandedTypeRecursively<Immutable<Mutate<S>>>;
+export type ExtractType<S> = expandedTypeRecursively<Immutable<S>>;
